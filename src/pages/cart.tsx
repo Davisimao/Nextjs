4e330688a5315f2724f2188f5ec05688aee7d326
Davi Sanches Simão <davi.simao@uscsonline.com.br>
2024-03-13 20:35:34 -0300

    first commit

---
+++ src/pages/cart.tsx
@@ -0,0 +1,21 @@
+// pages/cart.tsx
+
+import { NextPage } from "next";
+import Head from "next/head";
+
+export default function Cart() {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Carrinho
+      </h1>
+    </>
+  )
+}
+
---
+++ src/pages/products.tsx
@@ -0,0 +1,23 @@
+// pages/products.tsx
+
+
+import Head from "next/head";
+
+export default function Products() {
+
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Nossos Produtos
+      </h1>
+    </>
+  )
+
+
+}
